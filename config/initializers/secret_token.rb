8de73773f1f406c7caed3b97594a2507ef70ee68
user <user@user-HP-ProBook-4720s.(none)>
2012-12-28 12:57:08 +0400

    fuflo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ololo::Application.config.secret_token = '180f12d3211ab7ffaa5cfc7994030ac5b7190c00450db490a41b4a0177bf6905f4fc58491eb87359ba266de26c46a8f965d36f402364ef17e77f7b66edea7b42'
