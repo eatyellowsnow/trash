b44e264e7905f74df2a4f7249504753eaa3f28f4
eatyellowsnow <stepasalin@yandex.ru>
2012-12-28 13:40:11 +0400

    lihoigoiugh

--- 1.rb
+++ 1.rb
@@ -3,7 +3,7 @@ require "selenium-webdriver"
 
 driver = Selenium::WebDriver.for :firefox
 driver.manage.timeouts.implicit_wait = 30
-driver.navigate.to "http://172.18.8.251:3000"
+driver.navigate.to "http://0.0.0.0:3000"
 
 driver.find_element(:link, "About your applications environment").click
 driver.quit
