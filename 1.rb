1cc7185c5627aa08c59b28194a54b33517b7d457
eatyellowsnow <stepasalin@yandex.ru>
2013-01-16 18:14:22 +0400

    Update 1.rb

--- 1.rb
+++ 1.rb
@@ -1,9 +1,3 @@
 require "rubygems"
 require "selenium-webdriver"
 
-driver = Selenium::WebDriver.for :firefox
-driver.manage.timeouts.implicit_wait = 30
-driver.navigate.to "http://0.0.0.0:3000"
-
-driver.find_element(:link, "About your applications environment").click
-driver.quit
